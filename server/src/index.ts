b35f7d5cbca1a6e2a43a0e462b0a3aa64844bead
Michael Frieze <michaelfrieze@icloud.com>
2020-11-20 17:11:14 -0500

    server: setting up and configuring mikroORM | also making it all work with typescript

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,18 +1,17 @@
 import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+import mikroConfig from "./mikro-orm.config";
 
 const main = async () => {
-  const orm = await MikroORM.init({
-    dbName: "lireddit",
-    type: "postgresql",
-    debug: process.env.NODE_ENV !== "production",
-  });
+  const orm = await MikroORM.init(mikroConfig);
+
+  const post = orm.em.create(Post, { title: "my first post" });
+  await orm.em.persistAndFlush(post);
+  console.log("----------sql 2---------");
+  await orm.em.nativeInsert(Post, { title: "my first post 2" });
 };
 
-/* 
-setup postgress just by installing postgres app on macOS
-follow instructions
-
-then, in the CLI:
-createdb lireddit
-
-*/
+main().catch((err) => {
+  console.error(err);
+});
