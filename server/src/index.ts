3d2428f464d125bee65b232969b6cfce316a52fe
Michael Frieze <michaelfrieze@icloud.com>
2020-11-20 03:08:28 -0500

    started setting up postgreSQL and mikroORM

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,18 @@
-console.log("hello world, how are you?");
+import { MikroORM } from "@mikro-orm/core";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: process.env.NODE_ENV !== "production",
+  });
+};
+
+/* 
+setup postgress just by installing postgres app on macOS
+follow instructions
+
+then, in the CLI:
+createdb lireddit
+
+*/
