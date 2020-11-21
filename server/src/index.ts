26a27eb2bb92e30a0787863d93a4872f412b51ee
Michael Frieze <michaelfrieze@icloud.com>
2020-11-21 03:58:14 -0500

    server: using orm to find post and console logging them

--- server/src/index.ts
+++ server/src/index.ts
@@ -5,11 +5,12 @@ import mikroConfig from './mikro-orm.config';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
+  await orm.getMigrator().up();
+  // const post = orm.em.create(Post, { title: 'my first post' });
+  // await orm.em.persistAndFlush(post);
 
-  const post = orm.em.create(Post, { title: 'my first post' });
-  await orm.em.persistAndFlush(post);
-  console.log('----------sql 2---------');
-  await orm.em.nativeInsert(Post, { title: 'my first post 2' });
+  const posts = await orm.em.find(Post, {});
+  console.log(posts);
 };
 
 main().catch(err => {
