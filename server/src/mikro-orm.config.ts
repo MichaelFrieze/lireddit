f349fd331adf720daf3ddac50f257a9172653cbc
Michael Frieze <michaelfrieze@icloud.com>
2020-11-21 03:30:11 -0500

    server: created mikro-orm migration | npx mikro-orm migration:create

--- server/src/mikro-orm.config.ts
+++ server/src/mikro-orm.config.ts
@@ -1,10 +1,20 @@
 import { Post } from "./entities/Post";
 import { __prod__ } from "./constants";
 import { MikroORM } from "@mikro-orm/core";
+import path from "path";
 
 export default {
+  migrations: {
+    path: path.join(__dirname, "./migrations"), // path to the folder with migrations
+    pattern: /^[\w-]+\d+\.[tj]s$/, // regex pattern for the migration files
+  },
   entities: [Post],
   dbName: "lireddit",
   type: "postgresql",
   debug: !__prod__,
 } as Parameters<typeof MikroORM.init>[0];
+
+/* 
+https://youtu.be/I6ypD7qv3Z8?t=1770
+29:30
+*/
