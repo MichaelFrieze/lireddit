aaf6f1a2b51c0d8a7e2336cbafc9ef6cfc7efa5f
Michael Frieze <michaelfrieze@icloud.com>
2020-11-22 18:22:13 -0500

    just a quick update before I end for the day

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,16 +3,22 @@ import { __prod__ } from './constants';
 // import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
 import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { buildSchema } from 'type-graphql';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
 
   const app = express();
-  app.get('/', (_, res) => {
-    // ignore a param by using an underscore | was supposed to be request
-    res.send('hello');
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [],
+      validate: false,
+    }),
   });
+
   app.listen(4000, () => {
     console.log('server started on localhost:4000');
   });
@@ -23,6 +29,6 @@ main().catch(err => {
 });
 
 /* 
-https://youtu.be/I6ypD7qv3Z8?t=2439
-40:49
+https://youtu.be/I6ypD7qv3Z8?t=2659
+44:19
 */
