34ac542a885e32d7cc5bd76e8713cad8718b9ad3
Michael Frieze <michaelfrieze@icloud.com>
2020-11-22 16:51:38 -0500

    installed express apollo and graphql to start build the server

--- server/src/index.ts
+++ server/src/index.ts
@@ -9,10 +9,15 @@ const main = async () => {
   // const post = orm.em.create(Post, { title: 'my first post' });
   // await orm.em.persistAndFlush(post);
 
-  const posts = await orm.em.find(Post, {});
-  console.log(posts);
+  // const posts = await orm.em.find(Post, {});
+  // console.log(posts);
 };
 
 main().catch(err => {
   console.error(err);
 });
+
+/* 
+https://youtu.be/I6ypD7qv3Z8?t=2439
+40:49
+*/
