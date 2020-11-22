c184975b373ffef16fdd8d48e6296402e5e59885
Michael Frieze <michaelfrieze@icloud.com>
2020-11-22 16:58:09 -0500

    successfully setup a simple server that just says hello

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,21 @@
 import { MikroORM } from '@mikro-orm/core';
 import { __prod__ } from './constants';
-import { Post } from './entities/Post';
+// import { Post } from './entities/Post';
 import mikroConfig from './mikro-orm.config';
+import express from 'express';
 
 const main = async () => {
   const orm = await MikroORM.init(mikroConfig);
   await orm.getMigrator().up();
-  // const post = orm.em.create(Post, { title: 'my first post' });
-  // await orm.em.persistAndFlush(post);
 
-  // const posts = await orm.em.find(Post, {});
-  // console.log(posts);
+  const app = express();
+  app.get('/', (_, res) => {
+    // ignore a param by using an underscore | was supposed to be request
+    res.send('hello');
+  });
+  app.listen(4000, () => {
+    console.log('server started on localhost:4000');
+  });
 };
 
 main().catch(err => {
